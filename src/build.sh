6d436c448be4f6076a44e62ea7a9c93e4f97d0ac
Lexi <91022045+kwzuu@users.noreply.github.com>
2022-07-27 16:34:10 -0700

    use supported c++ version

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 fname="./jvm"
 
-g++ -Wall -Wextra -Wpedantic -Wshadow -Werror -std=c++23 -o "$fname" $(find . -name "*.cpp")
+g++ -Wall -Wextra -Wpedantic -Wshadow -Werror -std=c++20 -o "$fname" $(find . -name "*.cpp")
 
 if [[ "$1" = "run" ]]; then
   shift 1
