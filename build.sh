342937ed98e815055e149e9d03bd1e8459a2f94c
Luna! <51751618+janLuna@users.noreply.github.com>
2022-07-28 02:48:04 +0200

    very important change to build.sh
    meow

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 fname="./jvm"
 
+echo "MEOW MEOW MEOW MEOW..."
+
 g++ -Wall -Wextra -Wpedantic -Wshadow -Werror -std=c++20 -o "$fname" $(find . -name "*.cpp")
 
 if [[ "$1" = "run" ]]; then
